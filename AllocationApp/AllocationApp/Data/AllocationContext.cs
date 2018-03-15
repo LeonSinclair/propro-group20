a7d574136244ad186f2925d419b5a71dd3587f5d
James Sherlock <jsherloc@tcd.ie>
2018-03-15 12:46:50 +0000

    Here comes the error messages

---
+++ AllocationApp/AllocationApp/Data/AllocationContext.cs
@@ -0,0 +1,21 @@
+﻿using AllocationApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllocationApp.Data
+{
+    public class AllocationContext : DbContext
+    {
+        //this constructor is needed as when the context is being added in the ConfigureServices method in 
+        //startup, it takes the options specified in the startup (actually the SetupConfiguration method in program.cs) 
+        //and passes them into the context o that it knows what connection string to use 
+        public AllocationContext(DbContextOptions<AllocationContext> options): base(options)
+        {
+
+        }
+        public DbSet<Subordinates> Subordinates { get; set; }
+    }
+}
---
+++ AllocationApp/AllocationApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AllocationApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(SetupConfiguration)
+                .UseStartup<Startup>()
+                .Build();
+
+        private static void SetupConfiguration(WebHostBuilderContext ctx, IConfigurationBuilder builder)
+        {
+            //Removes the default configuration options so we can build it from the ground up
+            builder.Sources.Clear();
+            builder.AddJsonFile("config.json", false, true);
+        }
+    }
+}
