86a6007e72c99507b634b43c698a7e9c148ae0a4
Sulla Montes <sullavivia@gmail.com>
2018-03-16 00:37:10 +0000

    merged allocator to AllocatorApp

--- AllocationApp/AllocationApp/Controllers/HomeController.cs
+++ AllocationApp/AllocationApp/Controllers/HomeController.cs
@@ -15,6 +15,33 @@ namespace AllocationApp.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            ViewData["Message"] = "Your Dashboard page.";
+
+            return View();
+        }
+
+        public IActionResult Lecturer()
+        {
+            ViewData["Message"] = "Lecturer View.";
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
