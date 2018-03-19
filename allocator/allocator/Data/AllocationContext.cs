cbaa7656b88d8dbc6307a8a0868a05b178927ff8
triklin <lobgalszabolcs@gmail.com>
2018-03-19 23:09:00 +0000

    Fixed build with front end, added dependency files.

--- allocator/allocator/Data/AllocationContext.cs
+++ allocator/allocator/Data/AllocationContext.cs
@@ -1,21 +1,31 @@
-﻿using AllocationApp.Models;
-using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
-namespace AllocationApp.Data
+using Microsoft.EntityFrameworkCore;
+using allocator.Models;
+namespace allocator.Data
 {
-    public class AllocationContext : DbContext
+    public class AllocatorDBContext : DbContext
     {
-        //this constructor is needed as when the context is being added in the ConfigureServices method in 
-        //startup, it takes the options specified in the startup (actually the SetupConfiguration method in program.cs) 
-        //and passes them into the context o that it knows what connection string to use 
-        public AllocationContext(DbContextOptions<AllocationContext> options): base(options)
+        /*public AllocatorDBContext(DbContextOptions options) : base (options)
         {
 
+        }*/
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=AllocatorData;user=root;password=1234");
         }
-        public DbSet<Subordinates> Subordinates { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CourseWork>()
+                .HasKey(s => new { s.CourseId, s.UserId });
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
     }
 }
