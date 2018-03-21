802b9ea6a4de3597c816467d3a986664e7deb84c
James Sherlock <jsherloc@tcd.ie>
2018-03-21 23:12:32 +0000

    Adding model classes to deal with Many to Many relationships

---
+++ AllocationApp/AllocationApp/Models/Module.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllocationApp.Models
+{
+    public class Module
+    {
+        public String Id { get; set; }
+        public String Name { get; set; }
+    }
+}
