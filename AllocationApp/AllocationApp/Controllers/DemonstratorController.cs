d72621b2b8d02ea781dd87ea1b6ef5baeb508e8d
cjpdk <ckenny5@tcd.ie>
2018-03-22 23:41:49 +0000

    Skeleton files for demonstrator

---
+++ AllocationApp/AllocationApp/Controllers/DemonstratorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class DemonstratorController : Controller
+    {
+        // 
+        // GET: /Demonstrator/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Demonstrator/Welcome/ 
+        // Requires using System.Text.Encodings.Web
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+
+        // 
+        // GET: /Demonstrator/Rage/ 
+        public string Rage()
+        {
+            return "Fuck everything.";
+        }
+    }
+}
