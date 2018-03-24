3493a5717c5a8a2daaafaab6a1dc8daf26d2f855
Sulla Montes <sullavivia@gmail.com>
2018-03-24 21:12:07 +0000

    Updates on bank details functionalities

--- AllocationApp/AllocationApp/Controllers/SupervisorController.cs
+++ AllocationApp/AllocationApp/Controllers/SupervisorController.cs
@@ -38,7 +38,7 @@ namespace AllocationApp.Controllers
         [HttpPost("AddSubordinates")]
         public async Task<IActionResult> AddSubordinates([Bind("ID,FirstName,LastName,Occupation")]Subordinates model)
         {
-            if(ModelState.IsValid)//Server side validation
+            if (ModelState.IsValid)//Server side validation
             {
                 _context.Add(model);
                 await _context.SaveChangesAsync();
@@ -52,6 +52,69 @@ namespace AllocationApp.Controllers
         {
             return View();
         }
+
+        [HttpGet("Settings")]
+        public async Task<IActionResult> Settings(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Subordinates
+                .AsNoTracking()
+                .SingleOrDefaultAsync(m => m.ID == id);
+            
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View("Settings");
+        }
+
+        [HttpPost, ActionName("Settings")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SettingsPost(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Subordinates
+                .AsNoTracking()
+                .SingleOrDefaultAsync(m => m.ID == id);
+            
+            if (await TryUpdateModelAsync<Subordinates>(
+                user,
+                "",
+                i => i.FirstName, i => i.LastName))
+            {
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateException /* ex */)
+                {
+                    //Log the error (uncomment ex variable name and write a log.)
+                    ModelState.AddModelError("", "Unable to save changes. " +
+                        "Try again, and if the problem persists, " +
+                        "see your system administrator.");
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        public IActionResult BankDetails()
+        {
+            ViewData["Message"] = "Your Dashboard page.";
+
+            return View();
+        }
+
+
 
         public async Task<IActionResult> SubordinateDetails(int? id)
         {
@@ -97,69 +160,18 @@ namespace AllocationApp.Controllers
             return RedirectToAction(nameof(Subordinates));
         }
 
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.Subordinates
-                .Include(i => i.FirstName)
-                .AsNoTracking()
-                .SingleOrDefaultAsync(m => m.ID == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            return View(user);
-        }
-
-        [HttpPost, ActionName("Edit")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> EditUser(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.Subordinates
-                .Include(i => i.FirstName)
-                .AsNoTracking()
-                .SingleOrDefaultAsync(m => m.ID == id);
-
-            if (await TryUpdateModelAsync<Subordinates>(
-                user,
-                "",
-                i => i.FirstName, i => i.LastName))
-            {
-                try
-                {
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateException /* ex */)
-                {
-                    //Log the error (uncomment ex variable name and write a log.)
-                    ModelState.AddModelError("", "Unable to save changes. " +
-                        "Try again, and if the problem persists, " +
-                        "see your system administrator.");
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(user);
-        }
-
-        // Bank Details
+        // Bank Details - NOT WORKING YET
 
         [HttpGet("AddBankDetails")]
-        public IActionResult AddBankDetails()
+        public IActionResult BankDetails(int? id)
         {
+            var subordinate = _context.Subordinates.SingleOrDefault(m => m.ID == id);
+
             return View("BankDetails");
         }
 
         [HttpPost("AddBankDetails")]
-        public async Task<IActionResult> AddBankDetails([Bind("BankName,BankAddress,IBAN,SortCode")]Subordinates model)
+        public async Task<IActionResult> BankDetails([Bind("ID,BankName,BankAddress,IBAN,SortCode")]Subordinates model)
         {
             if(ModelState.IsValid)//Server side validation
             {
